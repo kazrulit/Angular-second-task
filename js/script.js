66dfc8033e0026523f24d24f750c6d06196cdf2f
kazrulit <mr_spec@mail.ru>
2016-02-10 16:24:57 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Khamid_Sarmanov on 2/10/2016.
+ */
+angular.module('myApp', []).controller('usersController', ['$scope', function($scope){
+
+    $scope.users = [
+        {firstName: 'John', lastName: 'Doe', attendStatus: 'Will be there'},
+    ]
+
+    /**
+     * Add form if valid
+     * @param firstName
+     * @param lastName
+     * @param email
+     * @param status
+     */
+    $scope.addUser = function(firstName, lastName, email, status) {
+        if($scope.form.$valid) {
+            $scope.users.push({firstName: firstName, lastName: lastName, attendStatus: status});
+            console.log($scope.users);
+        }
+    }
+}]);
